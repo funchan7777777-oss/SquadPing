36eb33df76d45ab04bd1e4d362137e0ba4096827
funchan666 <80337431+funchan666@users.noreply.github.com>
2026-07-06 11:35:10 +0800

    new

--- lib/shared/visuals/squad_ping_assets.dart
+++ lib/shared/visuals/squad_ping_assets.dart
@@ -29,4 +29,77 @@ abstract final class SquadPingAssets {
   static const tabEmblemInactive = 'assets/images/Match.png';
   static const tabForumActive = 'assets/images/Forum.png';
   static const tabForumInactive = 'assets/images/Nexus.png';
+
+  static const gameZoneHero = 'assets/images/Questline.png';
+  static const gameZonePanel = 'assets/images/Harbor.png';
+  static const gameZoneChatBackdrop = 'assets/images/List.png';
+  static const gameSegmentActive = 'assets/images/Legend.png';
+  static const chatSegmentActive = 'assets/images/Gallery.png';
+  static const chatSegmentInactive = 'assets/images/Mystic.png';
+  static const gameSegmentInactive = 'assets/images/Showcase.png';
+  static const hotGlyph = 'assets/images/Ranger.png';
+  static const sendGlyph = 'assets/images/Profile.png';
+
+  static const gameFortressCover = 'assets/images/Clip.png';
+  static const gameKingdomCover = 'assets/images/Highlight.png';
+  static const gameStrikeCover = 'assets/images/Broadcast.png';
+  static const gameOpsCover = 'assets/images/Matchmaking.png';
+  static const gameSpectatorCover = 'assets/images/Spectator.png';
+
+  static const chatArcadeLobby = 'assets/images/Talk/chat_arcade_lobby.png';
+  static const chatEsportsArena = 'assets/images/Talk/chat_esports_arena.png';
+  static const chatStrategyTable = 'assets/images/Talk/chat_strategy_table.png';
+  static const chatOpenWorld = 'assets/images/Talk/chat_open_world.png';
+  static const chatCardKeys = 'assets/images/Talk/chat_card_keys.png';
+  static const chatStreamerStation =
+      'assets/images/Talk/chat_streamer_station.png';
+
+  static const avatarMaleArcadeHost =
+      'assets/images/head/avatar_male_arcade_host.png';
+  static const avatarFemaleNeonCat =
+      'assets/images/head/avatar_female_neon_cat.png';
+  static const avatarMaleStreetSnap =
+      'assets/images/head/avatar_male_street_snap.png';
+  static const avatarFemalePinkHeadset =
+      'assets/images/head/avatar_female_pink_headset.png';
+  static const avatarMaleBlueRoom =
+      'assets/images/head/avatar_male_blue_room.png';
+  static const avatarFemaleBlondeHeadset =
+      'assets/images/head/avatar_female_blonde_headset.png';
+  static const avatarFemaleSunlitSelfie =
+      'assets/images/head/avatar_female_sunlit_selfie.png';
+  static const avatarMaleMaroonHeadset =
+      'assets/images/head/avatar_male_maroon_headset.png';
+  static const avatarMaleLoungePlayer =
+      'assets/images/head/avatar_male_lounge_player.png';
+  static const avatarMaleShadowProfile =
+      'assets/images/head/avatar_male_shadow_profile.png';
+  static const avatarMaleGlassesHeadset =
+      'assets/images/head/avatar_male_glasses_headset.png';
+  static const avatarFemaleWhiteHeadset =
+      'assets/images/head/avatar_female_white_headset.png';
+  static const avatarFemaleGlassesSoft =
+      'assets/images/head/avatar_female_glasses_soft.png';
+  static const avatarFemaleGrayHeadphones =
+      'assets/images/head/avatar_female_gray_headphones.png';
+  static const avatarMaleDaylightPlayer =
+      'assets/images/head/avatar_male_daylight_player.png';
+  static const avatarFemaleCompetitiveHeadset =
+      'assets/images/head/avatar_female_competitive_headset.png';
+  static const avatarFemaleGlassesLeopard =
+      'assets/images/head/avatar_female_glasses_leopard.png';
+  static const avatarFemalePinkGamer =
+      'assets/images/head/avatar_female_pink_gamer.png';
+  static const avatarMaleConsoleDen =
+      'assets/images/head/avatar_male_console_den.png';
+  static const avatarMaleSmilingController =
+      'assets/images/head/avatar_male_smiling_controller.png';
+  static const avatarMaleFocusController =
+      'assets/images/head/avatar_male_focus_controller.png';
+  static const avatarMaleBacklitStation =
+      'assets/images/head/avatar_male_backlit_station.png';
+  static const avatarFemaleNeckHeadphones =
+      'assets/images/head/avatar_female_neck_headphones.png';
+  static const avatarFemaleWhiteSleeveless =
+      'assets/images/head/avatar_female_white_sleeveless.png';
 }
